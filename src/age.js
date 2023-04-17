9e6f3dc883fee61752a8011a1992fa82b30f7670
Johna <j-verstraaten@hotmail.com>
2023-04-17 11:55:50 +0000

    Add files via upload

---
+++ src/age.js
@@ -0,0 +1,12 @@
+function age(day, month, year) {
+  const today = [new Date().getDate(), new Date().getMonth() + 1, new Date().getFullYear()];
+  const birthday = [day, month, year];
+
+  if (today[1] < birthday[1] || (today[1] == birthday[1] && today[0] < birthday[0])) {
+    return today[2] - birthday[2] - 1;
+  } else {
+    return today[2] - birthday[2];
+  }
+}
+
+module.exports = age;
